8bcbe417a8a95147c48bef0a399fd2ea2bd54d11
Chih-Yu Chen <cy.chen@roometric.com>
2024-07-16 15:35:09 +0200

    add debugging messages

--- libs/mve/image.h
+++ libs/mve/image.h
@@ -225,7 +225,11 @@ template <typename T>
 inline void
 Image<T>::fill_color (T const* color)
 {
+    std::cout << "Channels: " << this->c << std::endl;
+    std::cout << "Image size: " << (this->end() - this->begin()) << std::endl;
+
     for (T* ptr = this->begin(); ptr != this->end(); ptr += this->c)
+        std::cout << "Filling color: " << *color << std::endl;
         std::copy(color, color + this->c, ptr);
 }
 
